8124bbb7a1aa92e0c6e0faee94abeb8f1a45a036
uras-desktop <uo7@njit.edu>
2020-02-15 15:12:38 -0500

    Added level module

--- include/level.h
+++ include/level.h
@@ -7,19 +7,38 @@
 
 typedef struct
 {
-    Sprite *background;
-    SDL_Rect bounds;
+    Sprite *background;     /**<Background sprite*/
+    SDL_Rect bounds;        /**<Bounds*/
 }Level;
 
-
+/**
+ * @brief Creates level with bg and bounds
+ * @param backgroundFile Path to background file
+ * @param bounds Level bounds
+ * @return Object type of Level
+ * */
 Level *level_new(char *backgroundFile, SDL_Rect bounds);
+
+/**
+ * @brief Frees level and its sprite
+ * */
 void level_free(Level *level);
+
+/**
+ * @brief Draw level bg image and bounds
+ * */
 void level_draw(Level *level);
+
+/**
+ * @brief Get current level object
+ * @return Static type Level
+ * */
 Level *level_get_active();
 
 /**
  * @brief test if a circle is touching or exceeding the level bounds
+ * @return 1 if anything exceeds level bounds
  */
-Uint8 level_bounds_test_circle(Level *level, Vector2D center, float radius, Vector2D *normal);
+Uint8 level_bounds_test_circle(Level *level, Vector2D center, float radius);
 
 #endif
--- src/level.c
+++ src/level.c
@@ -3,11 +3,11 @@
 #include "level.h"
 
 
-static Level *THE_LEVEL = NULL;
+static Level *current_level = NULL;
 
 Level *level_get_active()
 {
-    return THE_LEVEL;
+    return current_level;
 }
 
 Level *level_new(char *backgroundFile, SDL_Rect bounds)
@@ -19,8 +19,11 @@ Level *level_new(char *backgroundFile, SDL_Rect bounds)
         return NULL;
     }
     level = (Level*)gfc_allocate_array(sizeof(Level), 1); //allocate array
+    if(!level)return NULL;
     level->background = gf2d_sprite_load_image(backgroundFile);
     gfc_rect_set(level->bounds, bounds.x, bounds.y, bounds.w, bounds.h);
+    current_level = level;
+    slog("Level created %d x %d", level->bounds.w, level->bounds.h);
     return level;
 }
 
@@ -28,6 +31,7 @@ void level_free(Level *level){
     if (!level)return;
     gf2d_sprite_free(level->background);
     free(level);
+    slog("Level freed %d x %d", level->bounds.w, level->bounds.h);
 }
 
 void level_draw(Level *level){
@@ -36,3 +40,32 @@ void level_draw(Level *level){
     gf2d_draw_rect(level->bounds, vector4d(0,255,0,255));
 }
 
+Uint8 level_bounds_test_circle(Level *level, Vector2D center, float radius)
+{
+    Uint8 hit = 0;
+    if (!level)
+    {
+        slog("no level provided for test");
+        return 0;
+    }
+    //slog("Level sprite %c", level->background);
+    if (center.x - radius < level->bounds.x)
+    {
+        hit = 1;
+        slog("hiy");
+    }
+    if (center.y - radius < level->bounds.y)
+    {
+        hit = 1;
+    }
+    if (center.x + radius > level->bounds.x + level->bounds.w)
+    {
+        hit = 1;
+    }
+    if (center.y - radius > level->bounds.y + level->bounds.h)
+    {
+        hit = 1;
+    }
+    
+    return hit;
+}
